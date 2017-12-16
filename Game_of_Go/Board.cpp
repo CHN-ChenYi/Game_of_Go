67514a6a8ddaaf770e5b567b74c453ccff296be2
godwings_thinkpad_desktop <godwings@outlook.com>
2017-12-16 20:58:28 +0800

    Finish constructor

--- Game_of_Go/Board.cpp
+++ Game_of_Go/Board.cpp
@@ -3,8 +3,20 @@
 
 
 Board::Board() {
+  size = 0;
 }
 
+Board::Board(const int &_size) {
+  size = _size;
+  
+  table = vector< vector<Cell*> >();
+  for (int i = 0; i < size; i++) {
+    table.push_back(vector<Cell*>());
+    for (int j = 0; j < size; j++)
+      table[i].push_back(new Cell);
+  }
+
+}
 
 Board::~Board() {
 }
--- Game_of_Go/Board.h
+++ Game_of_Go/Board.h
@@ -1,7 +1,23 @@
 #pragma once
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "Cell.h"
+
+#include <vector>
+using std::vector;
+
 class Board {
-public:
+  int size;
+  vector< vector<Cell*> > table;
+
+ public:
   Board();
+  Board(const int &_size);
   ~Board();
+
 };
 
+
+#endif  // !BOARD_H
