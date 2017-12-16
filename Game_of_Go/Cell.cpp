cdf19e60111493a9966bb89dce94260572052969
godwings_thinkpad_desktop <godwings@outlook.com>
2017-12-16 20:42:42 +0800

    Finish

--- Game_of_Go/Cell.cpp
+++ Game_of_Go/Cell.cpp
@@ -3,8 +3,14 @@
 
 
 Cell::Cell() {
+  owner = NULL;
+  dead_time = -1;
 }
 
+Cell::Cell(Player* const _owner) {
+  owner = _owner;
+  dead_time = -1;
+}
 
 Cell::~Cell() {
 }
--- Game_of_Go/Cell.h
+++ Game_of_Go/Cell.h
@@ -6,10 +6,33 @@
 #include "Player.h"
 
 class Cell {
-  
-public:
+  Player *owner;
+  short dead_time;
+
+ public:
   Cell();
+  Cell(Player* const _owner);
   ~Cell();
+
+  bool IsDead() {
+    return dead_time != -1;
+  }
+
+  bool PutToDeath(const int &_dead_time) {
+    if (dead_time != -1)
+      return false;
+    dead_time = _dead_time;
+    return true;
+  }
+
+  bool NeedToBeRemoved(const int &_now_time) {
+    return dead_time <= _now_time;
+  }
+
+  Player* GetOwner() {
+    return owner;
+  }
+
 };
 
 
