ea5c8af84ec2df9df7a21250b68af6434c735a04
godwings_thinkpad_desktop <godwings@outlook.com>
2017-12-16 20:27:19 +0800

    Add items

---
+++ Game_of_Go/Cell.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "Cell.h"
+
+
+Cell::Cell() {
+}
+
+
+Cell::~Cell() {
+}
---
+++ Game_of_Go/Cell.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef CELL_H
+#define CELL_H
+
+#include "Player.h"
+
+class Cell {
+  
+public:
+  Cell();
+  ~Cell();
+};
+
+
+#endif  // !CELL_H
