2897ec7cde224e0f8ec77aeb87dde110e635aec6
godwings_thinkpad_desktop <godwings@outlook.com>
2017-12-17 12:20:36 +0800

    Remove dead ones

--- Game_of_Go/Board.h
+++ Game_of_Go/Board.h
@@ -10,7 +10,17 @@ using std::vector;
 
 class Board {
   short size;
+  const int dx[4] = {-1, 1, 0, 0};
+  const int dy[4] = {0, 0, -1, 1};
   vector< vector<Cell*> > table;
+
+  void Flash(const int &_now_time);
+
+  void Calculate(const int &_now_time, const int &_x, const int _y);
+
+  void PutToDeath(const int &_dead_time, const int &_x, const int &_y);
+
+  bool CheckAlive(vector< vector<bool> > &_vis, const int &_x, const int _y);
 
  public:
   Board();
@@ -25,7 +35,7 @@ class Board {
     return table[_x][_y];
   }
 
-  bool PlayAt(const int &_x, const int &_y, Player *_player);
+  bool PlayAt(const int &_now_time, const int &_x, const int &_y, Player *_player);
 
 };
 
